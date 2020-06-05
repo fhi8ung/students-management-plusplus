a6bb90c36e7f5bdf76d8b1623135cea9065c825a
Nguyễn Phi Hùng <fhihung@hotmail.com>
2020-06-05 11:02:55 +0700

    .

--- src/vn/plusplus/sms/model/Student.java
+++ src/vn/plusplus/sms/model/Student.java
@@ -1,4 +1,67 @@
 package vn.plusplus.sms.model;
 
 public class Student {
+    private String fullName;
+    private int idName;
+    private String lopHoc;
+    private String ngaySinh;
+    private String diaChi;
+    private String hanhKiem;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getIdName() {
+        return idName;
+    }
+
+    public void setIdName(int idName) {
+        this.idName = idName;
+    }
+
+    public String getLopHoc() {
+        return lopHoc;
+    }
+
+    public void setLopHoc(String lopHoc) {
+        this.lopHoc = lopHoc;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getHanhKiem() {
+        return hanhKiem;
+    }
+
+    public void setHanhKiem(String hanhKiem) {
+        this.hanhKiem = hanhKiem;
+    }
+
+    public Student(String fullName, int idName, String lopHoc, String ngaySinh, String diaChi, String hanhKiem) {
+        this.fullName = fullName;
+        this.idName = idName;
+        this.lopHoc = lopHoc;
+        this.ngaySinh = ngaySinh;
+        this.diaChi = diaChi;
+        this.hanhKiem = hanhKiem;
+    }
 }
