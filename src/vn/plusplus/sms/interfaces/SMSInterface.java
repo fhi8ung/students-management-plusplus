4d79b4468ab65e117455e896746181ef572ebc0d
Nguyen Xuan Kiem <v.kiemnx@vinfast.vn>
2020-06-06 08:40:29 +0700

    Add interface

---
+++ src/vn/plusplus/sms/interfaces/SMSInterface.java
@@ -0,0 +1,8 @@
+package vn.plusplus.sms.interfaces;
+
+import vn.plusplus.sms.model.Student;
+
+public interface SMSInterface {
+
+    void saveStudentToDatabase(Student student);
+}
---
+++ src/vn/plusplus/sms/service/SMSService.java
@@ -0,0 +1,11 @@
+package vn.plusplus.sms.service;
+
+import vn.plusplus.sms.interfaces.SMSInterface;
+import vn.plusplus.sms.model.Student;
+
+public class SMSService implements SMSInterface {
+    @Override
+    public void saveStudentToDatabase(Student student) {
+
+    }
+}
