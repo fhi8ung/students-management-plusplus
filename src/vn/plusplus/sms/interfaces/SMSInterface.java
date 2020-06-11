79fd2e6a1d23525082ee45d5b414a2e65f7bef7f
kiemnx <kiemnguyenxuan@gmail.com>
2020-06-11 22:26:46 +0700

    Add interface

--- src/vn/plusplus/sms/interfaces/SMSInterface.java
+++ src/vn/plusplus/sms/interfaces/SMSInterface.java
@@ -1,8 +1,17 @@
 package vn.plusplus.sms.interfaces;
 
 import vn.plusplus.sms.model.Student;
+import vn.plusplus.sms.model.StudentScore;
+
+import java.util.List;
 
 public interface SMSInterface {
 
     void saveStudentToDatabase(Student student);
+
+    List<StudentScore> readAllStudentScore();
+
+    Student findStudentByIdName(String idName);
+
+
 }
--- src/vn/plusplus/sms/service/SMSService.java
+++ src/vn/plusplus/sms/service/SMSService.java
@@ -2,10 +2,23 @@ package vn.plusplus.sms.service;
 
 import vn.plusplus.sms.interfaces.SMSInterface;
 import vn.plusplus.sms.model.Student;
+import vn.plusplus.sms.model.StudentScore;
+
+import java.util.List;
 
 public class SMSService implements SMSInterface {
     @Override
     public void saveStudentToDatabase(Student student) {
 
+    }
+
+    @Override
+    public List<StudentScore> readAllStudentScore() {
+        return null;
+    }
+
+    @Override
+    public Student findStudentByIdName(String idName) {
+        return null;
     }
 }
