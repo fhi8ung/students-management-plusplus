49b0aa5b82ee8ce06d871ff1620b311fb1c4d2db
Nguyễn Phi Hùng <fhihung@hotmail.com>
2020-06-14 20:28:52 +0700

    Update SMSService.java

--- src/vn/plusplus/sms/service/SMSService.java
+++ src/vn/plusplus/sms/service/SMSService.java
@@ -4,12 +4,22 @@ import vn.plusplus.sms.interfaces.SMSInterface;
 import vn.plusplus.sms.model.Student;
 import vn.plusplus.sms.model.StudentScore;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class SMSService implements SMSInterface {
     @Override
     public void saveStudentToDatabase(Student student) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("D:\\Program Files\\Java\\students-management-plusplus\\data\\student.txt");
+            fileOutputStream.write(34);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
 
+        }
     }
 
     @Override
